642f40ce3de5817a946ec3d34abc26bf272bb1d4
VeKiMa <149431810+VeKiMa@users.noreply.github.com>
2023-12-07 17:35:00 +0100

    Update webshop.sql

--- webshop.sql
+++ webshop.sql
@@ -1,48 +1,56 @@
-﻿--drop database if exists webshop1;
---go
+use master;
+go
+drop database if exists WEBSHOP;
+go
 
---create database webshop1;
---go
+create database WEBSHOP;
+go
+alter database WEBSHOP collate Croatian_CI_AS;
+go
 
---use webshop1;
+use WEBSHOP;
 
---create table narudzba(
---sifra int not null primary key identity(1,1),
---kupac int not null,
---datumnarudzbe datetime,
---datumdostave datetime,
---placanje varchar(50) not null,
---ukupaniznos decimal(18,2)
---);
+create table narudzba(
+sifra int not null primary key identity(1,1),
+kupac int not null,
+datumnarudzbe datetime,
+datumdostave datetime,
+placanje varchar(50) not null,
+ukupaniznos decimal(18,2)
+);
+create table detaljinarudzbe(
+sifra int not null primary key identity,
+narudzba int,
+proizvod int not null,
+kolicina varchar(999), 
+cijena decimal(18,2)
+);
+create table kupac(
+sifra int not null primary key identity,
+ime varchar(50) not null,
+prezime varchar(50) not null,
+email varchar(100) not null,
+adresa varchar(100) not null,
+telefon varchar(20) 
+);
+create table proizvod(
+sifra int not null primary key identity,
+naziv varchar(50) not null,
+vrsta int not null,
+cijena decimal(18,2)
+);
 
---create table detaljinarudzbe(
---sifra int not null primary key identity,
---narudzba int,
---proizvod int not null,
---kolicina varchar(999), 
---cijena decimal(18,2)
---);
+create table vrste(
+sifra int not null primary key identity,
+naziv varchar(50),
+opis varchar(100)
+);
 
---create table kupac(
---sifra int not null primary key identity,
---ime varchar(50) not null,
---prezime varchar(50) not null,
---email varchar(100) not null,
---adresa varchar(100) not null,
---telefon varchar(20) 
---);
 
---create table proizvod(
---sifra int not null primary key identity,
---naziv varchar(50) not null,
---vrsta varchar(50) not null,
---cijena decimal(18,2)
---);
-
---alter table narudzba add foreign key (kupac) references kupac(sifra);
---alter table detaljinarudzbe add foreign key (narudzba) references narudzba(sifra);
---alter table detaljinarudzbe add foreign key (proizvod) references proizvod(sifra);
-
+alter table narudzba add foreign key (kupac) references kupac(sifra);
+alter table detaljinarudzbe add foreign key (narudzba) references narudzba(sifra);
+alter table detaljinarudzbe add foreign key (proizvod) references proizvod(sifra);
+alter table proizvod add foreign key (vrsta) references vrste(sifra);
 
 --insert into kupac(ime,prezime,email,adresa,telefon) values
 --('Marija','Mihelić','mmiheli14@gmail.com','Opatijska 26b','0989836373'),
@@ -53,6 +61,4 @@
 --('Janješić','Filip','filip.janjesic@gmail.com','Vukovarska1','0911234589'),
 --('Jović','Nataša','natasajovic238@gmail.com','Vukovarska1','0911234509');
 
-
---select ime,prezime,email,adresa,telefon from kupac;
 
